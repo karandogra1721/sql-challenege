3b19f86de94a3d71bd715263a07873d5bf6e1a43
karandogra1721 <karandogra17@gmail.com>
2024-02-01 13:21:33 -0800

    Adding SQL challenge files

---
+++ EmployeeSQL/Module_9/MyTableSchemata.sql
@@ -0,0 +1,61 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/ai36NB
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(255)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "last_name" varchar(255)   NOT NULL,
+    "first_name" varchar(255)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_data" data   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_no" varchar(255)   NOT NULL,
+    "title" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(255)   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salaries" int   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar(255)   NOT NULL,
+    "emp_no" int   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" varchar(255)   NOT NULL,
+    "title" varchar(255)   NOT NULL
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "departments" ADD CONSTRAINT "fk_departments_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "dept_emp" ("dept_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
